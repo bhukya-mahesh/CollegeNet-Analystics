b2aec7b91505cd85026837de654cc3b020645529
bhukya-mahesh <bhukyamahesh4546@gmail.com>
2026-06-29 15:09:17 +0530

    moved database file to Created_database file

---
+++ Database/Create_Database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE collegenet_analytics;
+
+DROP DATABASE IF EXISTS collegenet_analytics;
+CREATE DATABASE collegenet_analytics;
+USE collegenet_analytics;
+SELECT DATABASE() AS CurrentDatabase;
